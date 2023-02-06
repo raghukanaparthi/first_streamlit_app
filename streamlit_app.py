e495f4738fafcf6f7cd2ac238334a9c37585926b
raghukanaparthi <124547911+raghukanaparthi@users.noreply.github.com>
2023-02-05 22:36:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('my parents new health diner')
-streamlit.title('Breakfast')
+streamlit.header('Breakfast')
 streamlit.title('Omega 3 & Blueberry Oat Meal')
 streamlit.title('Kale, Spinach & Rocket Smoothie')
